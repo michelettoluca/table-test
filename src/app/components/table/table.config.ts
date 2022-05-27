46cd2f0cdd17abba1d861763ef768bc4b2984a56
Luca Micheletto <mch.luca00@gmail.com>
2022-05-27 16:27:25 +0200

    Added table component + refactor

--- src/app/components/table/table.config.ts
+++ src/app/components/table/table.config.ts
@@ -3,11 +3,11 @@ import { OrderType, TableAction, TableConfig } from "./table.types";
 export const users: TableConfig = {
     headers: [
         { key: "firstName", label: "First name" },
-        { key: "lastName", label: "Last name" },
+        { key: "lastName", label: "Last name" }
     ],
     order: {
         column: "firstName",
-        type: OrderType.asc,
+        type: OrderType.asc
     },
     filters: [
         "firstName",
